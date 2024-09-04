2e946e69e60ff4bdf59d8baf861d04bc02134e5e
Ing. Henry Guzmán <hguzman10@gmail.com>
2024-09-03 20:09:39 -0500

    swagger

---
+++ proyectos/fleteo/api/swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://raw.githubusercontent.com/hguzman/cars-ok.com/main/template.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
